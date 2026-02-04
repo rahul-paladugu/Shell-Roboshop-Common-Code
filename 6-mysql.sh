6763e83c19526f82b24dca114c536c8db12c44a4
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-05 10:43:33 +1300

    Mysql-config

--- 6-mysql.sh
+++ 6-mysql.sh
@@ -14,4 +14,4 @@ start_enable_service mysqld
 error_handler Start_mysqld_service
 mysql_secure_installation --set-root-pass RoboShop@1 &>>$log
 error_handler Set_root_password
-end_time Mongodb
+end_time Mysql
