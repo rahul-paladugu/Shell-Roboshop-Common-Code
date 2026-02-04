dd7999c5062d9c3c35c6467bd190949747fdf094
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-04 14:38:37 +1300

    mysql-config

---
+++ 6-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Call common script
+source ./common.sh
+#Check root access
+check_root_access
+#Creating logs directory
+logs_creation
+#Configure Mysql
+start_time
+dnf install mysql-server -y &>>$log
+error_handler Instally_mysql_server
+start_enable_service mysqld
+error_handler Start_mysqld_service
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$log
+error_handler Set_root_password
+end_time Mongodb
