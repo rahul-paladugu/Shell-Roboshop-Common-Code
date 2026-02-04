dd62fef7284c48be39f2cfd26fa382c7b672f010
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-05 10:16:55 +1300

    instances-config

--- 0-instances.sh
+++ 0-instances.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#Call common script
+source ./common.sh
+#Variables for instance
 ami="ami-0220d79f3f480ecf5"
 sg="sg-03b441e0ba008f925"
 zone="Z0711084A6IKM873A3LI"
@@ -6,6 +10,12 @@ record="rscloudservices.icu"
 region="us-east-1"
 
 instances="mongodb catalogue redis user cart mysql shipping rabbitmq payment dispatch frontend"
+
+#Logs creation
+logs_path="/var/logs/shell-roboshop/"
+script_name=$(echo "$0" | cut -d "." -f1)
+log="$logs_path/$script_name.log"
+
 
 error_validation(){
     if [ $? -ne 0 ]; then
@@ -19,23 +29,23 @@ error_validation(){
 for instance in $instances
 do
  start_time=$(date +%s)
- echo "Creating AWS instance $instance as requested"
+ echo "${yellow}Creating AWS instance $instance as requested${reset}"
 #Creating an instance and collecting instance_ID into a variable
  instance_id=$(aws ec2 run-instances --image-id $ami --instance-type t3.micro  --security-group-ids $sg  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
  end_time=$(date +%s)
- echo "$instance instance is created successfully"
- echo "Time taken to create instance is $(($end_time - $start_time))Seconds"
+ echo "${yellow}$instance instance is created successfully${reset}"
+ echo "${blue}Time taken to create instance is $(($end_time - $start_time))Seconds.${reset}"
 # Wait until instance is running
 aws ec2 wait instance-running --region "$region" --instance-ids "$instance_id"
 #selecting IP address based on instance component
  if [ $instance = frontend ]; then
   ip=$(aws ec2 describe-instances --region us-east-1 --filters "Name=instance-id,Values=$instance_id" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-  record_name=$record
+  record_name=$record &>>$log
  else
   ip=$(aws ec2 describe-instances --region us-east-1 --filters "Name=instance-id,Values=$instance_id" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-  record_name="$instance.$record"
+  record_name="$instance.$record" &>>$log
  fi
-echo "ip for $instance is $ip"
+echo "${yellow}ip for $instance is $ip ${reset}"
 sleep 5
 #Creating R53 records for the instance created above
  aws route53 change-resource-record-sets --hosted-zone-id "$zone" --change-batch ' {
@@ -56,8 +66,8 @@ sleep 5
         }
     ]
 }
-'
+' &>>$log
 error_validation r53_records
-echo "The r53 record for $instance is $instance.$record"
+echo "${yellow}The r53 record for $instance is $instance.$record${reset}"
 done
 
