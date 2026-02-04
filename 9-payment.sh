38df7187229d892847bcc9b73ee51ce903a84909
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-05 11:16:35 +1300

    Payment-config

---
+++ 9-payment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Call common script
+source ./common.sh
+#Check root access
+check_root_access
+#Creating logs directory
+logs_creation
+#Configure Payment
+dnf install python3 gcc python3-devel -y &>>$log
+error_handler Installing_python
+system_user
+error_handler Creating_system_user
+download_code payment
+error_handler Downloading_payment_code
+pip3 install -r requirements.txt &>>$log
+error_handler Installing_dependencies
+#Setup Payment Service
+cp $script_dir/payment.service /etc/systemd/system/payment.service
+error_handler Downloading_payment_service
+systemctl daemon-reload &>>$log
+error_handler Daemon_reload
+start_enable_service payment
+error_handler Start_payment_service
+end_time Payment
