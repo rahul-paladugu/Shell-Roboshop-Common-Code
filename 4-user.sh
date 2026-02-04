9026f9d140ff4dd2e0c548bc2d1e1244b61e168b
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-04 14:32:06 +1300

    user-config

--- 4-user.sh
+++ 4-user.sh
@@ -6,31 +6,24 @@ source ./common.sh
 check_root_access
 #Creating logs directory
 logs_creation
-#Configure Catalogue
+#Configure user
 start_time
 dnf module disable nodejs -y &>>$log
-error_handler Disable_existing_nodejs
+error_handler Disabling_default_nodejs
 dnf module enable nodejs:20 -y &>>$log
-error_handler Enabling_nodejs_20_Version
+error_handler Enabling_nodejs_20
 dnf install nodejs -y &>>$log
-error_handler Install_nodejs
-system_user
-error_handler System_user_creation
-download_code catalogue
-error_handler Downloading_code
+error_handler Installing_nodejs_20_version
+System_user
+error_handler Creating_system_user
+Downloading_code user
+error_handler Downloading_user_code
 npm install &>>$log
-error_handler Instaiing_dependencies
-#Redis Service Setup
-dnf module disable redis -y &>>$log
-error_handler Disable_default_redis
-dnf module enable redis:7 -y &>>$log
-error_handler Enable_redis_7
-dnf install redis -y &>>$log
-error_handler Installing_redis_version_7
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
-error_handler Binding_ip
-sed -i '111s/protected-mode yes/protected-mode no/' /etc/redis/redis.conf
-error_handler Disabling_protected_mode
-start_enable_service redis
-error_handler start_and_enable_redis
+error_handler Installing_dependencies
+#User service setup
+cp $script_dir/user.service /etc/systemd/system/user.service
+systemctl daemon-reload &>>$log
+error_handler Daemon_reload
+start_enable_service user
+error_handler Start_user_service
 end_time
