ac707e9453ebe6d31ee15aa225717135fd2ef897
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-04 14:40:49 +1300

    shipping-config

---
+++ 7-shipping.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Call common script
+source ./common.sh
+#Check root access
+check_root_access
+#Creating logs directory
+logs_creation
+#Configure Shipping
+start_time
+
+
+
+
+
+end_time
